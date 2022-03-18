baaa9b22116b0fb4fcaac3f408f3e420d3c66d46
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-03-18 18:35:46 +0000

    Making test server run

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -11,23 +11,23 @@ export function searchLessons(req: Request, res: Response) {
 
     const queryParams = req.query;
 
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder || 'asc',
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize) || 3;
+    const courseId = queryParams['courseId'],
+          filter = queryParams['filter'] || '',
+          sortOrder = queryParams['sortOrder'] || 'asc',
+          pageNumber = parseInt(queryParams['pageNumber'] as string) || 0,
+          pageSize = parseInt(queryParams['pageSize'] as string) || 3;
 
     let lessons;
 
     if (courseId) {
-     lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
+        lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == parseInt(courseId as string)).sort((l1, l2) => l1.id - l2.id);
     }
     else {
         lessons = Object.values(LESSONS);
     }
 
     if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search((filter as string).toLowerCase()) >= 0);
     }
 
     if (sortOrder == "desc") {
--- server/server.ts
+++ server/server.ts
@@ -24,6 +24,7 @@ app.route('/api/login').post(loginUser);
 
 const httpServer = app.listen(9000, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address()["port"]);
+    console.log(httpServer.address());
 });
 
 
