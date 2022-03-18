140de9fa598874338a9f1c4f87d94c21a7d91a1a
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-03-18 16:28:10 +0000

    Init course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-angular';
+export class AppComponent implements  OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+
+    }
+
+  logout() {
+
+  }
+
 }
